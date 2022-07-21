6196203100c32c74d9d4732014cecc01015994fd
Silver <testingbugsrus@gmail.com>
2022-07-21 18:48:43 +0300

    add selector

--- src/test/java/com/youtube/pages/BeforeYouContinuePage.java
+++ src/test/java/com/youtube/pages/BeforeYouContinuePage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.youtube.helpers.Waiting.SIX_SEC;
 
 public class BeforeYouContinuePage {
-	private final SelenideElement rejectAllElement = $("#dialog yt-formatted-string #text");
+	private final SelenideElement rejectAllElement = $("tp-yt-paper-dialog #dialog yt-formatted-string #text");
 
 	public void clickOnRejectAll(){
 		rejectAllElement.shouldBe(Condition.exist, Duration.ofSeconds(SIX_SEC.getValue())).click();
