4fddcfbded04735d1e03cc88ac8a5e7672959c6b
Silver <testingbugsrus@gmail.com>
2022-07-21 19:33:43 +0300

    add selector

--- src/test/java/com/youtube/pages/BeforeYouContinuePage.java
+++ src/test/java/com/youtube/pages/BeforeYouContinuePage.java
@@ -12,7 +12,8 @@ import static com.youtube.helpers.Waiting.SIX_SEC;
 public class BeforeYouContinuePage {
 	//private final SelenideElement rejectAllElement = $("tp-yt-paper-dialog #dialog yt-formatted-string #text");
 	//private final SelenideElement rejectAllElement = $("ytd-app tp-yt-paper-dialog tp-yt-paper-button #text");
-	private final SelenideElement rejectAllElement = $x("//tp-yt-paper-dialog[@id='dialog']//yt-formatted-string[text()='Reject all']");
+	//private final SelenideElement rejectAllElement = $x("//tp-yt-paper-dialog[@id='dialog']//yt-formatted-string[text()='Reject all']");
+	private final SelenideElement rejectAllElement = $x("//*[text()='Reject all']");
 
 	public void clickOnRejectAll(){
 		rejectAllElement.shouldBe(Condition.exist, Duration.ofSeconds(SIX_SEC.getValue()))
