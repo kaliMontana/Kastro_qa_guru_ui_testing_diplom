b2336997eec255af32e480c1d03a5a8bdb22cc59
Silver <testingbugsrus@gmail.com>
2022-12-10 11:56:47 +0300

    Refactoring in Attach class

--- src/test/java/com/youtube/helpers/Attach.java
+++ src/test/java/com/youtube/helpers/Attach.java
@@ -12,22 +12,27 @@ import java.nio.charset.StandardCharsets;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class Attach {
-	@Attachment(value = "{attachName}", type = "text/plain")
+	private static final String TEXT_PLAIN_TYPE = "text/plain";
+	private static final String TEXT_HTML_TYPE = "text/html";
+	private static final String IMAGE_PNG_TYPE = "image/png";
+
+
+	@Attachment(value = "{attachName}", type = TEXT_PLAIN_TYPE)
 	public static String attachAsText(String attachName, String message) {
 		return message;
 	}
 
-	@Attachment(value = "Page source", type = "text/plain")
+	@Attachment(value = "Page source", type = TEXT_HTML_TYPE)
 	public static byte[] pageSource() {
 		return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
 	}
 
-	@Attachment(value = "{attachName}", type = "image/png")
+	@Attachment(value = "{attachName}", type = IMAGE_PNG_TYPE)
 	public static byte[] screenshotAs(String attachName) {
 		return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
 	}
 
-	@Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+	@Attachment(value = "Video", type = TEXT_HTML_TYPE, fileExtension = ".html")
 	public static String addVideo() {
 		return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
 				+ getVideoUrl(getSessionId())
